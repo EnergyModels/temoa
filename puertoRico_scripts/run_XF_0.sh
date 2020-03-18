3ed3b8c84abedd53a7ca767fb7bb14bb518c6375
EnergyModels <jab6ft@virginia.edu>
2020-03-18 16:18:18 -0400

    added puerto rico input files

--- puertoRico_scripts/run_XF_0.sh
+++ puertoRico_scripts/run_XF_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH --cpus-per-task=1
-#SBATCH -t 10:00:00
+#SBATCH --cpus-per-task=6
+#SBATCH -t 12:00:00
 #SBATCH -p standard
 
 cd ..
@@ -10,7 +10,7 @@ module purge
 module load anaconda/2019.10-py3.7
 
 # activate temoa environment
-source activate temoa-stoch-py3
+source activate temoa-py3
 
 # if gurobi is available
 module load gurobi/9.0.1
