9bbdd548b16d2710445c9448e713fa66f5b0a402
EnergyModels <jab6ft@virginia.edu>
2020-03-18 15:59:30 -0400

    added input files for puerto rico analysis

--- puertoRico_scripts/run_ZD_0.sh
+++ puertoRico_scripts/run_ZD_0.sh
@@ -7,20 +7,22 @@
 cd ..
 
 module purge
-module load anaconda/5.2.0-py2.7
+module load anaconda/2019.10-py3.7
 
 # activate temoa environment
-source activate temoa-stoch-py2
+source activate temoa-stoch-py3
 
 # if gurobi is available
-module load gurobi
+module load gurobi/9.0.1
 
 # set the NUM_PROCS env variable for the Python script
 export NUM_PROCS=$SLURM_CPUS_PER_TASK
 
 # run
 cd tools
+cd stochastics
 python generate_scenario_tree_JB.py options/stoch_ZD_0.py --debug
 python rewrite_tree_nodes.py options/stoch_ZD_0.py --debug
+cd ..
 cd ..
 python temoa_model/temoa_stochastic.py --config=temoa_model/config_stoch_ZD_0.txt
